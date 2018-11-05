95a44f2bd839c7534dd15e051628ebc7902721a5
Shawn <shpach@github.com>
2018-11-04 21:29:31 -0500

    Add arbitrary accuracy and loss function

--- model.py
+++ model.py
@@ -1,11 +1,11 @@
-from tf.keras.applications.vgg19 import VGG19, preprocess_input
+from keras.applications.vgg19 import VGG19, preprocess_input
 from keras.models import Model, Input
 from keras import backend as K
 
 class SsdModel(object):
 	""""""
 	def __init__(self):
-		raw_images = Input(shape = (224,224))
+		raw_images = Input(shape = (224,224,3))
 		vgg19 = VGG19(weights='imagenet', include_top=False, pooling=None)
 		for layer in vgg19.layers:
 			layer.trainable = False
@@ -16,15 +16,20 @@ class SsdModel(object):
 		output = feats_extracted
 
 		ssd_model = Model(raw_images, output)
+
+		def ssd_loss(y_true, y_pred):
+			return K.sum(K.log(y_true) - K.log(y_pred))
+
+		def accuracy(y_true, y_pred):
+			return K.sum(K.log(y_true) - K.log(y_pred))
+
 		ssd_model.compile(optimizer = "adam", loss = ssd_loss, metrics = ["accuracy", accuracy])
+		vgg19.summary()
+		ssd_model.summary()
 		self.model = ssd_model
 
 
-	def ssd_loss(y_true, y_pred):
-		pass
-
-	def accuracy(y_true, y_pred):
-		pass
+		
 
 	def preprocess(X):
 		X = preprocess_input(X)
